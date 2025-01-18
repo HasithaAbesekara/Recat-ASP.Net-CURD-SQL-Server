9532aa066b80aed2b655189d414f62306012e78e
A.M.H.M Abesekara <89085725+HasithaAbesekara@users.noreply.github.com>
2025-01-18 22:16:58 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="w-screen py-20 flex justify-center flex-col items-center">
+      <div className=" flex items-center justify-center gap-1 mb-5">
+        <h1 className="text-4xl font-bold">
+          Recat AND ASP.Net Core CURD Recat AND ASP.Net Core CURD
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
