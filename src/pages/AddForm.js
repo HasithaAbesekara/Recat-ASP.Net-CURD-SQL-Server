467d123ac3579cc2664bd0f8c1cefb56a20c0657
A.M.H.M Abesekara <89085725+HasithaAbesekara@users.noreply.github.com>
2025-01-19 00:33:10 +0530

    Create recat part

---
+++ src/pages/AddForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddForm = () => {
+  return (
+    <div className=" max">
+      <h1></h1>
+    </div>
+  );
+};
+
+export default AddForm;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -7,6 +8,16 @@ const Home = () => {
         <h1 className="text-4xl font-bold">
           Recat AND ASP.Net Core CURD Recat AND ASP.Net Core CURD
         </h1>
+      
+      </div>
+      <div className=" overflow-x-auto py-10">
+        <div className=" mb-2 w-full text-right">
+          <Link to="/create">
+            <button className=" text-white bg-purple-600 hover:bg-gray-200 hover:text-purple-600 front-medium border border-red-50 py-5 px-3 ">
+              Add New User
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
